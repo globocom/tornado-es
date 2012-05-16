9ba4970bf5ffff22274f49d3342f85030196ee04
nsigustavo <nsigustavo@gmail.com>
2012-05-16 18:11:50 -0300

    renomeado get para search e modificado parametro value para source esperando um dicionario com query

--- tornadoes/__init__.py
+++ tornadoes/__init__.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 from tornado.httpclient import AsyncHTTPClient
 from tornado.ioloop import IOLoop
+from urllib import urlencode
 
-class ESConnection:
+class ESConnection(object):
     
     def __init__(self, host='localhost', port= '9200', io_loop=None):
         self.io_loop = io_loop or IOLoop.instance()
         self.host = host
         self.port = port
         self.client = AsyncHTTPClient(self.io_loop)
-        
-    def get(self, callback, **kwargs):
+
+    def search(self, callback, **kwargs):
         index = kwargs.get('index', '_all')
         type_ = '/' + kwargs.get('type') if kwargs.has_key('type') else ''
-        field = kwargs.get('field','_all') + ':'
-        value = kwargs.get('value', '*')
         size = kwargs.get('size', 10)
         page = kwargs.get('page', 1)
-        begin = (page-1)*size
-        
-        path = "/%(index)s%(type)s/_search?q=%(field)s%(value)s&from=%(begin)d&size=%(size)d" % {"index": index, "type": type_, "field": field, 
-                                                                                                  "value": value, "begin": begin, "size":size}
+        from_ = (page-1)*size
+        source = json.dumps( kwargs.get('source', {"query":{"query_string" : {"query" : "*"}}}))
+        path = "/%(index)s%(type)s/_search?%(querystring)s" % {
+                    "querystring":urlencode({'source':source, 'from': from_, 'size': size}),
+                    "index":index,
+                    "type": type_
+                    }
         self.get_by_path(path, callback)
-        
+
     def get_by_path(self, path, callback):
         url = 'http://%(host)s:%(porta)s%(path)s' % {"host": self.host, "porta": self.port, "path": path}
         self.client.fetch(url, callback)
--- tornadoes/testes/unit/test_tornadoes.py
+++ tornadoes/testes/unit/test_tornadoes.py
@@ -3,8 +3,11 @@
 import unittest2
 import json
 from tornadoes import ESConnection
+
+from tornado import escape
 from tornado.testing import AsyncTestCase
 from tornado.ioloop import IOLoop
+
 
 class TestESConnection(AsyncTestCase):
     
@@ -19,56 +22,54 @@ class TestESConnection(AsyncTestCase):
         
     def tratar_resposta_consulta_simples(self, response):
         assert response.code == 200
-        resposta = json.loads(response.body)
+        resposta = escape.json_decode(response.body)
         assert resposta["hits"]["total"] == 1
-        assert resposta["hits"]["hits"][0]["_id"] == u'http://g1.be.globoi.com/noticia/1/fast'
+        assert resposta["hits"]["hits"][0]["_id"] == u'http://g1.be.globoi.com/noticia/2/fast'
         self.stop()
         
     def tratar_resposta_consulta_especificando_tipo_campo(self, response):
-        assert response.code == 200
-        resposta = json.loads(response.body)
-        assert resposta["hits"]["total"] == 1
-        assert resposta["hits"]["hits"][0]["_id"] == u'http://g1.be.globoi.com/noticia/1/fast'
+        resposta = escape.json_decode(response.body)
+        assert response.code == 200, resposta
+        assert resposta["hits"]["total"] == 1, resposta
+        assert resposta["hits"]["hits"][0]["_id"] == u'171171', resposta["hits"]["hits"][0]["_id"]+'\n'+str(resposta)
         self.stop()
         
     def test_consulta_simples(self):
-        self.es_connection.get_by_path("/_search?q=_id:http\:\/\/g1.be.globoi.com\/noticia\/1\/fast", self.tratar_resposta_consulta_simples)
+        self.es_connection.get_by_path("/_search?q=_id:http\:\/\/g1.be.globoi.com\/noticia\/2\/fast", self.tratar_resposta_consulta_simples)
         self.wait()
-        
+
     def test_consulta_especificando_tipo_campo(self):
-        self.es_connection.get(self.tratar_resposta_consulta_especificando_tipo_campo, index="teste", value="http\:\/\/g1.be.globoi.com\/noticia\/1\/fast", 
-                                      type="materia", field="_id")
+        self.es_connection.search(callback=self.tratar_resposta_consulta_especificando_tipo_campo,
+            source={"query": {"text" : {"ID" : "171171"}}},
+            type="materia", index="teste")
         self.wait()
-    
+
     def verificar_quantidade_de_registros(self, response):
-        assert response.code == 200
-        resposta = json.loads(response.body)
+        resposta = escape.json_decode(response.body)
+        assert response.code == 200, "response.code != 200\n" + str(resposta)
         assert resposta["hits"]["total"] == 28
         self.stop()
     
     def test_consulta_todos_os_registros(self):
-        self.es_connection.get(self.verificar_quantidade_de_registros)
+        self.es_connection.search(self.verificar_quantidade_de_registros)
         self.wait()
         
-        
     def verificar_busca_indice_especifico(self, response):
-        assert response.code == 200
-        resposta = json.loads(response.body)
+        resposta = escape.json_decode(response.body)
+        assert response.code == 200, "response.code != 200\n" + resposta
         assert resposta["hits"]["total"] == 14
         self.stop()
     
     def test_busca_indice_especifico(self):
-        self.es_connection.get(self.verificar_busca_indice_especifico, index="outroteste")
+        self.es_connection.search(callback=self.verificar_busca_indice_especifico, index="outroteste")
         self.wait()
         
     def verificar_busca_tipo_especifico(self, response):
-        assert response.code == 200
-        resposta = json.loads(response.body)
+        resposta = escape.json_decode(response.body)
+        assert response.code == 200, "response.code != 200\n" + str(resposta)
         assert resposta["hits"]["total"] == 2
         self.stop()
     
     def test_busca_tipo_especifico(self):
-        self.es_connection.get(self.verificar_busca_tipo_especifico, type='galeria')
+        self.es_connection.search(self.verificar_busca_tipo_especifico, type='galeria')
         self.wait()
-        
-        
